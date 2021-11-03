5083484e3840bac3264c0417161d6761a91a383e
Fabio Marques <fabio2001marques@gmail.com>
2021-11-03 16:27:20 +0000

    Criado o layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace TrabalhoFinalProgInternet.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
