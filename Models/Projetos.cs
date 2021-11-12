47407649c6a478c2f4d7a8958366e33bc424db12
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2021-11-12 09:36:39 +0000

    Criação da Class/tabelaProjetosw

---
+++ Models/Projetos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabalhoFinalProgInternet.Models
+{
+    public class Projetos
+    {
+        public int ProjetosID { get; set; }
+    }
+}
