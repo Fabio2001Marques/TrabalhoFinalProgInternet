f8d6b8eadd6de020084ddd58fb9c5b3316cfd733
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2021-11-12 09:45:24 +0000

    Criação dos campos

--- Models/Projetos.cs
+++ Models/Projetos.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +10,12 @@ namespace TrabalhoFinalProgInternet.Models
     public class Projetos
     {
         public int ProjetosID { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        [DisplayName("Nome do projeto")]
+        public string Name { get; set; }
+
+
     }
 }
