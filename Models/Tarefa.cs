5f2960171d86d01cf72d3cdf75f66f4d388c90ab
Rafael Santos <rdsrafael@gmail.com>
2021-11-19 09:45:22 +0000

    Revisão do modelo "Tarefa"

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -24,6 +24,11 @@ namespace TrabalhoFinalProgInternet.Models
         [DisplayName("Data de Início")]
         public DateTime DataDeInicio { get; set; }
 
+        [Required(ErrorMessage = "Preencha o campo Data de início")]
+        [DataType(DataType.Date, ErrorMessage = "Insira uma data válida")]
+        [DisplayName("Data Prevista de Finalização")]
+        public DateTime DataPrevista { get; set; }
+
         [DataType(DataType.Date, ErrorMessage = "Insira uma data válida")]
         [DisplayName("Data de Finalização")]
         public DateTime DataDeFim { get; set; }
@@ -31,6 +36,6 @@ namespace TrabalhoFinalProgInternet.Models
         [DisplayName ("Projeto")]
         public int ProjetoId { get; set; }
 
-        public Projeto NomeProjeto { get; set; }
+        public Projeto Projeto { get; set; }
     }
 }
