a04558f4bec7ac8765fb5a9bc53682dcd6884766
Rafael Santos <rdsrafael@gmail.com>
2021-12-03 10:00:10 +0000

    Criada a migração tarefas

---
+++ Data/GestorProjetosMigrations/20211203095838_Tarefas.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrabalhoFinalProgInternet.Data.GestorProjetosMigrations
+{
+    public partial class Tarefas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataFim",
+                table: "Tarefa",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataInicio",
+                table: "Tarefa",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataPrevistaFim",
+                table: "Tarefa",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataPrevistaInicio",
+                table: "Tarefa",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataFim",
+                table: "Tarefa");
+
+            migrationBuilder.DropColumn(
+                name: "DataInicio",
+                table: "Tarefa");
+
+            migrationBuilder.DropColumn(
+                name: "DataPrevistaFim",
+                table: "Tarefa");
+
+            migrationBuilder.DropColumn(
+                name: "DataPrevistaInicio",
+                table: "Tarefa");
+    }
+}
