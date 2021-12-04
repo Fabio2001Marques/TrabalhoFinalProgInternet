64a144a35e4081bac7ab4936e2a16f0c462c2561
Fabio Marques <fabio2001marques@gmail.com>
2021-12-04 12:48:44 +0000

    Criada uma função para preencher cada tabela

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -10,69 +10,71 @@ namespace TrabalhoFinalProgInternet.Data
     {
         internal static void Populate(GestorProjetosContext gestorContext)
         {
-            //// Preencher Tabela Cargo
-            //for (int i = 1; i <= 100; i++){
-            //    gestorContext.Cargo.Add(
-            //    new Cargo
-            //    {
-            //        Nome = "Cargo" + i
-            //    }
-            //    ) ;
-            //}
-            //gestorContext.SaveChanges();
-            //Cargo cargo = gestorContext.Cargo.FirstOrDefault();
-
-            //// Preencher Tabela Colaboradores
-            //for (int i = 1; i <= 100; i++)
-            //{
-            //    gestorContext.Colaborador.Add(
-            //    new Colaborador
-            //    {
-            //        Nome = "Teste" + i,
-            //        Contacto = "923654178",
-            //        Email = "teste"+i+"@gmail.com",
-            //        NumeroCC = "12365478",
-            //        Cargo = cargo
-            //    }
-            //    );
-            //}
-            //gestorContext.SaveChanges();
-
-            ////== Preencher Tabela Cargo com dados reais
-
-
-            //gestorContext.Cargo.Add(new Cargo { Nome = "Gestor" });
-            //gestorContext.Cargo.Add(new Cargo { Nome = "Programador" });
-            //gestorContext.Cargo.Add(new Cargo { Nome = "Tester" });
-            //gestorContext.SaveChanges();
-
-            ////== Preencher Tabela Colaborador com dados reais
-
-            //gestorContext.Colaborador.Add(new Colaborador
-            //{
-            //    Nome = "Fábio Marques",
-            //    Contacto = "923654178",
-            //    Email = "fabio2001marques@gmail.com",
-            //    NumeroCC = "12365478",
-            //    Cargo = gestorContext.Cargo.Find(1)
-            //});
-            //gestorContext.Colaborador.Add(new Colaborador
-            //{
-            //    Nome = "Diogo Saraiva",
-            //    Contacto = "912547896",
-            //    Email = "diogosaraiva@gmail.com",
-            //    NumeroCC = "32654178",
-            //    Cargo = gestorContext.Cargo.Find(2)
-            //});
-            //gestorContext.Colaborador.Add(new Colaborador
-            //{
-            //    Nome = "Rafael Santos",
-            //    Contacto = "965874123",
-            //    Email = "rafaelsantos@gmail.com",
-            //    NumeroCC = "12547896",
-            //    Cargo = gestorContext.Cargo.Find(3)
-            //});
-            //gestorContext.SaveChanges();
-            //}
+            
+            PreencherDadosFicticiosCargo(gestorContext);
+            PreencherDadosFicticiosColaboradores(gestorContext);
+            PreencherDadosReaisCargo(gestorContext);
+            PreencherDadosReaisColaborador(gestorContext);
+     
         }
-    }
+        private static void PreencherDadosFicticiosCargo(GestorProjetosContext gestorContext)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                gestorContext.Cargo.Add(new Cargo{Nome = "Cargo" + i});
+            }
+            gestorContext.SaveChanges();
+        }
+        private static void PreencherDadosFicticiosColaboradores(GestorProjetosContext gestorContext)
+        {
+            Cargo cargo = gestorContext.Cargo.FirstOrDefault();
+            for (int i = 1; i <= 100; i++)
+            {
+                gestorContext.Colaborador.Add(new Colaborador{
+                    Nome = "Teste" + i,
+                    Contacto = "923654178",
+                    Email = "teste" + i + "@gmail.com",
+                    NumeroCC = "12365478",
+                    Cargo = cargo});
+            }
+            gestorContext.SaveChanges();
+        }
+        private static void PreencherDadosReaisCargo(GestorProjetosContext gestorContext)
+        {
+            gestorContext.Cargo.Add(new Cargo { Nome = "Gestor" });
+            gestorContext.Cargo.Add(new Cargo { Nome = "Programador" });
+            gestorContext.Cargo.Add(new Cargo { Nome = "Tester" });
+            gestorContext.SaveChanges();
+        }
+        private static void PreencherDadosReaisColaborador(GestorProjetosContext gestorContext)
+        {
+            gestorContext.Colaborador.Add(new Colaborador{
+                Nome = "Fábio Marques",
+                Contacto = "923654178",
+                Email = "fabio2001marques@gmail.com",
+                NumeroCC = "12365478",
+                Cargo = gestorContext.Cargo.Find(1)});
+
+            gestorContext.Colaborador.Add(new Colaborador{
+                Nome = "Diogo Saraiva",
+                Contacto = "912547896",
+                Email = "diogosaraiva@gmail.com",
+                NumeroCC = "32654178",
+                Cargo = gestorContext.Cargo.Find(2)});
+
+            gestorContext.Colaborador.Add(new Colaborador{
+                Nome = "Rafael Santos",
+                Contacto = "965874123",
+                Email = "rafaelsantos@gmail.com",
+                NumeroCC = "12547896",
+                Cargo = gestorContext.Cargo.Find(3)});
+
+            gestorContext.SaveChanges();
+        }
+
+
+
+    }    
+}
+
+
