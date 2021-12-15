ba3dbc5d50ba54f8a2f02d805e529198619be227
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2021-12-15 09:13:50 +0000

    Criar o ProjetoListViewModel

---
+++ ViewModels/ProjetoListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrabalhoFinalProgInternet.Models;
+
+namespace TrabalhoFinalProgInternet.ViewModels
+{
+    public class ProjetoListViewModel
+    {
+        public IEnumerable<Projeto> Projetos { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string ProcuraNome { get; set; }
+    }
+}
