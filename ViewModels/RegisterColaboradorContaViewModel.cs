985aa791d5471d5d8ebd56277afefb514a3e7021
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2022-01-21 10:13:33 +0000

    Mudar o colaboradorconta register para ler a palavra pass

--- ViewModels/RegisterColaboradorContaViewModel.cs
+++ ViewModels/RegisterColaboradorContaViewModel.cs
@@ -23,8 +23,12 @@ namespace TrabalhoFinalProgInternet.ViewModels
         public string Phone { get; set; }
 
         [Required]
+        [StringLength(100, ErrorMessage ="The {0} deve ser pelo menos {2} e no maximo {1} carateres.",MinimumLength = 6)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
-        [Required]
+       
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirma a palavra-passe")]
         [Compare("Password",ErrorMessage = "As palavras-chave teem de coincidir")]
         public string ConfirmPassword { get; set; }
     }
