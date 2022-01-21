e7d55bb3cb0ef675518f35fe97393b5c003670e0
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2022-01-21 09:53:24 +0000

    Criar o ViewModel do ColaboradorConta

---
+++ ViewModels/RegisterColaboradorContaViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabalhoFinalProgInternet.ViewModels
+{
+    public class RegisterColaboradorContaViewModel
+    {
+        public int ColaboradorContaId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password",ErrorMessage = "As palavras-chave teem de coincidir")]
+        public string ConfirmPassword { get; set; }
+    }
+}
