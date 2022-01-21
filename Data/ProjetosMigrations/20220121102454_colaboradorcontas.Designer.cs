efa65192019fca530d6f5ba827e84f1be734e4f9
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2022-01-21 10:25:58 +0000

    Adicionado a migração add-migration colaboradorcontas -context gestor -o .\Data.\ProjetoMigrations

--- Data/ProjetosMigrations/20220121102454_colaboradorcontas.Designer.cs
+++ Data/ProjetosMigrations/20220121102454_colaboradorcontas.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrabalhoFinalProgInternet.Data;
 
-namespace TrabalhoFinalProgInternet.Data.GestorProjetosMigrations
+namespace TrabalhoFinalProgInternet.Data.ProjetosMigrations
 {
     [DbContext(typeof(GestorProjetosContext))]
-    partial class GestorProjetosContextModelSnapshot : ModelSnapshot
+    [Migration("20220121102454_colaboradorcontas")]
+    partial class colaboradorcontas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
