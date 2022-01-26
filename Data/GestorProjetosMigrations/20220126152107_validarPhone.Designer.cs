c13235d9cf9672c8fe149f9c4f08cac0f3a6f5ee
Fabio Marques <fabio2001marques@gmail.com>
2022-01-26 15:24:00 +0000

    Adicionadas funções Regex para Validar CC e Número de Telefone.

--- Data/GestorProjetosMigrations/20220126152107_validarPhone.Designer.cs
+++ Data/GestorProjetosMigrations/20220126152107_validarPhone.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrabalhoFinalProgInternet.Data;
 
 namespace TrabalhoFinalProgInternet.Data.GestorProjetosMigrations
 {
     [DbContext(typeof(GestorProjetosContext))]
-    partial class GestorProjetosContextModelSnapshot : ModelSnapshot
+    [Migration("20220126152107_validarPhone")]
+    partial class validarPhone
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
